cb540cd6d3fee9efa3cbb6a6f5f5bf49cebefbcd
Kim Kijung <rlwjd1504@o365.hufs.ac.kr>
2020-09-21 13:19:31 +0900

    Added bigger map, simple camera, and sea wave effect

--- basicmap.cpp
+++ basicmap.cpp
@@ -3,21 +3,26 @@
 
 HRESULT basicmap::init()
 {
-	//IMAGEMANAGER->addImage("watertile", "Images/이미지/타일/img_tile_water.bmp", TILESIZE, TILESIZE);
-	//IMAGEMANAGER->addFrameImage("plaintile", "Images/이미지/타일/img_tile_plain.bmp", TILESIZE, TILESIZE, 4, 1);
-	//IMAGEMANAGER->addFrameImage("plainedge", "Images/이미지/타일/img_tile_plainEdge.bmp", TILESIZE, TILESIZE, 4, 2, true, RGB(255, 0, 255));
 	watertile = new image;
 	watertile->init("Images/이미지/타일/img_tile_water.bmp", TILESIZE, TILESIZE, 1, 1);
 	plaintile = new image;
 	plaintile->init("Images/이미지/타일/img_tile_plain.bmp", 224, 56, 4, 1);
 	plainedge = new image;
 	plainedge->init("Images/이미지/타일/img_tile_plainEdge.bmp", 224, 112, 4, 2, true, RGB(255, 0, 255));
+	wave = new image;
+	wave->init("Images/이미지/타일/img_tile_wave.bmp", 56, 56);	
+	underwater = new image;
+	underwater->init("Images/이미지/타일/img_tile_bottomGround.bmp", 56, 56, true, RGB(255,0,255));
 
-	for (int i = 0; i < TILEY; i++) {
-		for (int j = 0; j < TILEX; j++) {
-			_tiles[i][j].rc = RectMake(0 + j * TILESIZE, 0 + i * TILESIZE, TILESIZE, TILESIZE);
-			_tiles[i][j].level = TERRAIN;
-			_tiles[i][j].terrain = watertile;
+	for (int i = 0; i < MAPTILEY; i++) {
+		for (int j = 0; j < MAPTILEX; j++) {
+			tile _tile;
+			_tile.rc = RectMake(-(MAPTILEX*TILESIZE/2) + j * TILESIZE, -(MAPTILEY*TILESIZE / 2) + i * TILESIZE, TILESIZE, TILESIZE);
+			_tile.level = TERRAIN;
+			_tile.terrain = watertile;
+			_tile.terrainFrameX = 0;
+			_tile.terrainFrameY = 0;
+			_vTiles.push_back(_tile);
 		}
 	}
 	return S_OK;
@@ -31,19 +36,21 @@ void basicmap::release()
 	SAFE_DELETE(plaintile);
 	plainedge->release();
 	SAFE_DELETE(plainedge);
+	wave->release();
+	SAFE_DELETE(wave);
+	underwater->release();
+	SAFE_DELETE(underwater);
 }
 
 void basicmap::update()
 {
-
 	if (INPUT->GetKey(VK_LBUTTON)) {
-		for (int i = 0; i < TILEY; i++) {
+		for (int i = 0; i < TILEY*MAPY; i++) {
 			bool stop = false;
-			for (int j = 0; j < TILEX; j++) {
-				_tiles[i][j].rc;
-				RECT temp;
-				if (PtInRect(&_tiles[i][j].rc, _ptMouse)) {
-					_tiles[i][j].terrain = plaintile;
+			for (int j = 0; j < MAPTILEX; j++) {
+				if (PtInRect(&_vTiles[i*MAPTILEY + j].rc, _ptMouse)) {
+					_vTiles[i*MAPTILEY + j].terrain = plaintile;
+					_vTiles[i*MAPTILEY + j].terrainFrameX = RANDOM->range(4);
 					stop = true;
 					break;
 				}
@@ -52,13 +59,12 @@ void basicmap::update()
 		}
 	}
 	if (INPUT->GetKey(VK_RBUTTON)) {
-		for (int i = 0; i < TILEY; i++) {
+		for (int i = 0; i < MAPTILEY; i++) {
 			bool stop = false;
-			for (int j = 0; j < TILEX; j++) {
-				_tiles[i][j].rc;
-				RECT temp;
-				if (PtInRect(&_tiles[i][j].rc, _ptMouse)) {
-					_tiles[i][j].terrain = watertile;
+			for (int j = 0; j < MAPTILEX; j++) {
+				if (PtInRect(&_vTiles[i*MAPTILEY + j].rc, _ptMouse)) {
+					_vTiles[i*MAPTILEY + j].terrain = watertile;
+					_vTiles[i*MAPTILEY + j].terrainFrameX = 0;
 					stop = true;
 					break;
 				}
@@ -66,51 +72,98 @@ void basicmap::update()
 			if (stop) break;
 		}
 	}
+	// 카메라 이동
+	if (INPUT->GetKey(VK_LEFT))
+	{
+		for (int i = 0; i < MAPTILEY; i++) {
+			for (int j = 0; j < MAPTILEX; j++) {
+				_vTiles[i*MAPTILEY + j].rc.left += 10;
+				_vTiles[i*MAPTILEY + j].rc.right += 10;
+			}
+		}
+	}
+	if (INPUT->GetKey(VK_RIGHT))
+	{
+		for (int i = 0; i < MAPTILEY; i++) {
+			for (int j = 0; j < MAPTILEX; j++) {
+				_vTiles[i*MAPTILEY + j].rc.left -= 10;
+				_vTiles[i*MAPTILEY + j].rc.right -= 10;
+			}
+		}
+	}
+	if (INPUT->GetKey(VK_UP))
+	{
+		for (int i = 0; i < MAPTILEY; i++) {
+			for (int j = 0; j < MAPTILEX; j++) {
+				_vTiles[i*MAPTILEY + j].rc.top += 10;
+				_vTiles[i*MAPTILEY + j].rc.bottom += 10;
+			}
+		}
+	}
+	if (INPUT->GetKey(VK_DOWN))
+	{
+		for (int i = 0; i < MAPTILEY; i++) {
+			for (int j = 0; j < MAPTILEX; j++) {
+				_vTiles[i*MAPTILEY + j].rc.top -= 10;
+				_vTiles[i*MAPTILEY + j].rc.bottom -= 10;
+			}
+		}
+	}
 }
 
 void basicmap::render()
 {
-	for (int i = 0; i < TILEY; i++) {
-		for (int j = 0; j < TILEX; j++) {
-			if (_tiles[i][j].terrain == watertile) {
-				_tiles[i][j].terrain->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 0, 0);
+	for (int i = 0; i < MAPTILEY; i++) {
+		for (int j = 0; j < MAPTILEX; j++) {
+			if (_vTiles[i*MAPTILEY + j].terrain == watertile) {
+				_vTiles[i*MAPTILEY + j].terrain->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, _vTiles[i*MAPTILEY + j].terrainFrameX, _vTiles[i*MAPTILEY + j].terrainFrameY);
 			}
-			else {
-				_tiles[i][j].terrain->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 0, 0);
+		}
+	}
+	for (int i = 0; i < MAPTILEY; i++) {
+		for (int j = 0; j < MAPTILEX; j++) {
+			if (_vTiles[i*MAPTILEY + j].terrain != watertile) {
+				_vTiles[i*MAPTILEY + j].terrain->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, _vTiles[i*MAPTILEY + j].terrainFrameX, _vTiles[i*MAPTILEY + j].terrainFrameY);
 				//서북 꼭짓점
-				if (i - 1 >= 0 && j - 1 >= 0 && _tiles[i - 1][j - 1].terrain == watertile) {
-					plainedge->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 0, 1);
+				if (j - 1 >= 0 && _vTiles[(i - 1)*MAPTILEY + (j - 1)].terrain == watertile) {
+					plainedge->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, 0, 1);
 				}
 				//동북 꼭짓점
-				if (i - 1 >= 0 && j + 1 < TILEX && _tiles[i - 1][j + 1].terrain == watertile) {
-					plainedge->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 1, 1);
+				if (i - 1 >= 0 && j + 1 < MAPTILEX && _vTiles[(i - 1)*MAPTILEY +(j + 1)].terrain == watertile) {
+					plainedge->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, 1, 1);
 				}
 				//동남 꼭짓점
-				if (i + 1 < TILEY && j + 1 < TILEX && _tiles[i + 1][j + 1].terrain == watertile) {
-					plainedge->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 2, 1);
+				if (i + 1 < MAPTILEY && j + 1 < MAPTILEX && _vTiles[(i + 1)*MAPTILEY +(j + 1)].terrain == watertile) {
+					plainedge->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, 2, 1);
 				}
 				//서남 꼭짓점
-				if (i + 1 < TILEY && j - 1 >= 0 && _tiles[i + 1][j - 1].terrain == watertile) {
-					plainedge->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 3, 1);
+				if (i + 1 < MAPTILEY && j - 1 >= 0 && _vTiles[(i + 1)*MAPTILEY +(j - 1)].terrain == watertile) {
+					plainedge->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, 3, 1);
 				}
 				//서
-				if (j-1 >= 0 && _tiles[i][j-1].terrain == watertile) {
-					plainedge->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 0, 0);
+				if (j-1 >= 0 && _vTiles[i*MAPTILEY +(j - 1)].terrain == watertile) {
+					plainedge->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, 0, 0);
 				}
 				//동
-				if (j + 1 < TILEX && _tiles[i][j + 1].terrain == watertile) {
-					plainedge->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 1, 0);
+				if (j + 1 < MAPTILEX && _vTiles[i*MAPTILEY +(j + 1)].terrain == watertile) {
+					plainedge->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, 1, 0);
 				}
 				//북
-				if (i - 1 >= 0 && _tiles[i-1][j].terrain == watertile) {
-					plainedge->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 2, 0);
+				if (i - 1 >= 0 && _vTiles[(i - 1)*MAPTILEY +j].terrain == watertile) {
+					plainedge->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, 2, 0);
 				}
 				//남
-				if (i + 1 < TILEY && _tiles[i + 1][j].terrain == watertile) {
-					plainedge->frameRender(getMemDC(), _tiles[i][j].rc.left, _tiles[i][j].rc.top, 3, 0);
+				if (i + 1 < MAPTILEY && _vTiles[(i + 1)*MAPTILEY +j].terrain == watertile) {
+					plainedge->frameRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.top, 3, 0);
+					underwater->render(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.bottom);
+					wave->alphaRender(getMemDC(), _vTiles[i*MAPTILEY + j].rc.left, _vTiles[i*MAPTILEY + j].rc.bottom+20+RANDOM->range(2), 100);
+
 				}
 
 			}
 		}
 	}
+
+	string pos = "좌표: " + to_string(_ptMouse.x) + ", " + to_string(_ptMouse.y);
+	textOut(getMemDC(), 0, 0, pos.c_str());
 }
--- basicmap.h
+++ basicmap.h
@@ -1,8 +1,13 @@
 #pragma once
 #include "gameNode.h"
-#define TILEX 10
-#define TILEY 10
+#define TILEX 12
+#define TILEY 12
 #define TILESIZE 56
+#define MAPX 7
+#define MAPY 7
+#define MAPSIZE MAPX*MAPY
+#define MAPTILEX MAPX*TILEX
+#define MAPTILEY MAPY*TILEY
 enum GROUNDLEVEL {
 	TERRAIN, OBJECT
 };
@@ -11,6 +16,11 @@ struct tile {
 	GROUNDLEVEL level;
 	image* terrain;
 	image* object;
+
+	int terrainFrameX;
+	int terrainFrameY;
+	int objFrameX;
+	int objFrameY;
 };
 class basicmap : public gameNode
 {
@@ -18,8 +28,11 @@ private:
 	image* watertile;
 	image* plaintile;
 	image* plainedge;
+	image* wave;
+	image* underwater;
 private:
 	tile _tiles[TILEY][TILEX];
+	vector<tile> _vTiles;
 public:
 	HRESULT init();
 	void release();
