ef97772fea2ffe698c689b1b04fafdcca6b13fd0
유인 한 <gksyy123@naver.com>
2020-10-12 23:20:33 +0900

    플레이어 레벨시, 스테미나 최대치 증가

--- unit.h
+++ unit.h
@@ -43,7 +43,7 @@ public:
 	virtual void render(HDC hdc) override;
 	
 public:
-	void hurt(int damage);			// 체력 닳음
+	virtual void hurt(int damage);			// 체력 닳음
 	virtual void animation();		// 애니메이션 연출
 	virtual void collision() { ; };	// 충돌시 연출 (필드 아이템)
 	virtual void dead() { ; };		// 사망시 연출
