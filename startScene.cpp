f4bf936a9d8fa0325621e3df39e30a883cd455bf
Kim Kijung <rlwjd1504@o365.hufs.ac.kr>
2020-10-15 17:12:15 +0900

    fixed it be buildable after merge

--- startScene.cpp
+++ startScene.cpp
@@ -172,29 +172,30 @@ void startScene::CheckButtonClick()
 					case BTN::CREDIT:	SCENEMANAGER->loadScene("게임 화면"); break;	// 크레딧 창
 					case BTN::EXIT:		SCENEMANAGER->loadScene("게임 화면"); break;	// 게임 종료
 
-				case BTN::SLOT1:	
-					
-					SAVEMANAGER->My_Game_save_file_item = "item_save1.map";
-					SAVEMANAGER->My_Game_save_file_equip = "equip_save1.map";
-					SAVEMANAGER->My_Game_save_file_tile = "tile_save1.map";
-					SCENEMANAGER->loadScene("게임 화면");
-					break;	// 게임 슬롯 1
-				
-				case BTN::SLOT2:	
+					case BTN::SLOT1:
 
-					SAVEMANAGER->My_Game_save_file_item = "item_save2.map";
-					SAVEMANAGER->My_Game_save_file_equip = "equip_save2.map";
-					SAVEMANAGER->My_Game_save_file_tile = "tile_save2.map";
-					SCENEMANAGER->loadScene("게임 화면");
+						SAVEMANAGER->My_Game_save_file_item = "item_save1.map";
+						SAVEMANAGER->My_Game_save_file_equip = "equip_save1.map";
+						SAVEMANAGER->My_Game_save_file_tile = "tile_save1.map";
+						SCENEMANAGER->loadScene("게임 화면");
+						break;	// 게임 슬롯 1
 
-					break;	// 게임 슬롯 2
-				case BTN::SLOT3:	
+					case BTN::SLOT2:
 
-					SAVEMANAGER->My_Game_save_file_item = "item_save3.map";
-					SAVEMANAGER->My_Game_save_file_equip = "equip_save3.map";
-					SAVEMANAGER->My_Game_save_file_tile = "tile_save3.map";
-					SCENEMANAGER->loadScene("게임 화면");
-					break;	// 게임 슬롯 3
+						SAVEMANAGER->My_Game_save_file_item = "item_save2.map";
+						SAVEMANAGER->My_Game_save_file_equip = "equip_save2.map";
+						SAVEMANAGER->My_Game_save_file_tile = "tile_save2.map";
+						SCENEMANAGER->loadScene("게임 화면");
+
+						break;	// 게임 슬롯 2
+					case BTN::SLOT3:
+
+						SAVEMANAGER->My_Game_save_file_item = "item_save3.map";
+						SAVEMANAGER->My_Game_save_file_equip = "equip_save3.map";
+						SAVEMANAGER->My_Game_save_file_tile = "tile_save3.map";
+						SCENEMANAGER->loadScene("게임 화면");
+						break;	// 게임 슬롯 3
+					}
 				}
 			}
 		}
